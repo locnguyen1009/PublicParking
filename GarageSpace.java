246e62d0c7ccf44e5bdeac472200b5e2c2e1f654
locnguyen1009 <101686246+locnguyen1009@users.noreply.github.com>
2022-07-28 14:34:38 -0500

    Update GarageSpace.java

--- GarageSpace.java
+++ GarageSpace.java
@@ -2,5 +2,7 @@ public class GarageSpace {
     public static void main(String[] args) {
         System.out.println("hello!");
         System.out.println("Hello");
+        System.out.println("change from github");
+        
     }
 }
